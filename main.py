f3d1934ae097cc587ad22c6deffad9bebec9d179
Brich40 <brich.oussama@gmail.com>
2020-12-17 21:03:59 +0100

    Order RGB lists

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ import png
 
 
 def encode(input_path, output_path):
+    rgb_rows = []
+
     r = png.Reader(input_path)
     print(r.read()[2])
-    for row in r.asRGB()[2]:
-        print(list(row))
 
-# def get_rgb_ordered(rgb_rows):
+    for row in r.asRGB()[2]:
+        rgb_rows.append(list(row))
+    get_rgb_ordered(rgb_rows)
+
+
+# Order RGB list (as : [(R,G,B), (R,G,B), ...]
+def get_rgb_ordered(rgb_rows):
+    rgb_pixels_list = []
+    for row in rgb_rows:
+        rgb_pixels = [row[x:x + 3] for x in range(0, len(row), 3)]
+        rgb_pixels_list.append(rgb_pixels)
+    print(rgb_pixels_list)
+    return rgb_pixels_list
 
 
 #################
