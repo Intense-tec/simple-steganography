b59fd028a4bfc01f51526fdaf31d4d324293a163
Brich40 <brich.oussama@gmail.com>
2020-12-17 21:21:59 +0100

    Get ascii code

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 """
 Name : main.py.py
 Author : OBR01
-Contect : brich.oussama@gmail.com
+Contact : brich.oussama@gmail.com
 Time    : 17/12/2020 16:16
 Desc:
 """
 import png
 
 
-def encode(input_path, output_path):
+# Encode text in input image and save it with the change
+def encode(input_path, text, output_path):
     rgb_rows = []
 
     r = png.Reader(input_path)
@@ -17,6 +18,17 @@ def encode(input_path, output_path):
     for row in r.asRGB()[2]:
         rgb_rows.append(list(row))
     get_rgb_ordered(rgb_rows)
+
+    ascii = convert_text_to_ascii("Hello Alice !")
+    print(ascii)
+
+
+# Convert text to ascii list
+def convert_text_to_ascii(text):
+    ascii_list = []
+    for char in text:
+        ascii_list.append(ord(char))
+    return ascii_list
 
 
 # Order RGB list (as : [(R,G,B), (R,G,B), ...]
@@ -33,9 +45,10 @@ def get_rgb_ordered(rgb_rows):
 # Main Function #
 #################
 if __name__ == "__main__":
-    print("Staring...")
+
     image_input_path = "./images/basketBall.png"
     image_output_path = ""
+    encode_text = "Hello Oussama"
 
-    encode(image_input_path, image_output_path)
+    encode(image_input_path, encode_text, image_output_path)
 
