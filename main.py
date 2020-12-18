a140e3484d184d84431eee017cbdd5af0cf37b22
Brich40 <brich.oussama@gmail.com>
2020-12-18 09:25:46 +0100

    Decode done :)

--- main.py
+++ main.py
@@ -13,14 +13,91 @@ def encode(input_path, text, output_path):
     rgb_rows = []
 
     r = png.Reader(input_path)
-    print(r.read()[2])
-
     for row in r.asRGB()[2]:
         rgb_rows.append(list(row))
-    get_rgb_ordered(rgb_rows)
+    # print(rgb_rows)
+    # rgb_pixels_list = get_rgb_ordered(rgb_rows)
 
     ascii = convert_text_to_ascii("Hello Alice !")
-    print(ascii)
+
+    print(rgb_rows[0])
+    for char in ascii:
+        print(bin(char)[2:].zfill(8))
+
+    '''
+    Loop in ascii values of char
+    and change the r/b/g value to even if binary is 1
+    if not then change the r/b/g value to odd    
+    '''
+    pixel_rgb_position = 0
+    row = 0
+    column = 0
+
+    for index, char_ascii in enumerate(ascii):
+        char_binaries = bin(char_ascii)[2:].zfill(8)
+        for binary in char_binaries:
+            row = pixel_rgb_position // len(rgb_rows)
+            column = pixel_rgb_position % len(rgb_rows)
+            if binary == "0" and (rgb_rows[row][column] % 2) != 0:
+                rgb_rows[row][column] = rgb_rows[row][column] - 1
+            elif binary == "1" and (rgb_rows[row][column] % 2) == 0:
+                rgb_rows[row][column] = rgb_rows[row][column] - 1
+            pixel_rgb_position += 1
+
+        '''
+        Add separation sign : change r/g/b to odd (value-1) 
+        '''
+        row = pixel_rgb_position // len(rgb_rows)
+        column = pixel_rgb_position % len(rgb_rows)
+        if len(ascii) > index:
+            if (rgb_rows[row][column] % 2) == 0:
+                rgb_rows[row][column] = rgb_rows[row][column] - 1
+        else:
+            if (rgb_rows[row][column] % 2) != 0:
+                rgb_rows[row][column] = rgb_rows[row][column] - 1
+        pixel_rgb_position += 1
+
+    print(rgb_rows[0])
+    return rgb_rows
+
+
+# Decode text from image
+def decode(image_path, rgb_rows):
+    text = ""
+    binaries = []
+    is_text_end = False
+
+    for rgb_row in rgb_rows:
+        binary = ""
+        rgb_position = 0
+        binary_count = 0
+
+        # If end of text encrypted in image then break
+        if is_text_end:
+            break
+
+        # Loop in r/g/b values
+        for value in rgb_row:
+            # If end of binary block (8 r/g/b values)
+            if binary_count == 8:
+                if (value % 2) == 0:
+                    is_text_end = True
+                    break
+                else:
+                    # Append char binary
+                    binaries.append(binary)
+                    binary = ""
+                    binary_count = 0
+                    # ignore separation
+                    continue
+
+            if (value % 2) == 0:
+                binary += "0"
+            else:
+                binary += "1"
+
+            binary_count += 1
+    print(str(binaries))
 
 
 # Convert text to ascii list
@@ -31,14 +108,20 @@ def convert_text_to_ascii(text):
     return ascii_list
 
 
+# Convert ascii list decoded to text
+def convert_ascii_to_text(ascii_list):
+    text = ""
+    for ascii in ascii_list:
+        text += chr(ascii)
+    return text
+
 # Order RGB list (as : [(R,G,B), (R,G,B), ...]
-def get_rgb_ordered(rgb_rows):
-    rgb_pixels_list = []
-    for row in rgb_rows:
-        rgb_pixels = [row[x:x + 3] for x in range(0, len(row), 3)]
-        rgb_pixels_list.append(rgb_pixels)
-    print(rgb_pixels_list)
-    return rgb_pixels_list
+# def get_rgb_ordered(rgb_rows):
+#     rgb_pixels_list = []
+#     for row in rgb_rows:
+#         rgb_pixels = [row[x:x + 3] for x in range(0, len(row), 3)]
+#         rgb_pixels_list.append(rgb_pixels)
+#     return rgb_pixels_list
 
 
 #################
@@ -50,5 +133,5 @@ if __name__ == "__main__":
     image_output_path = ""
     encode_text = "Hello Oussama"
 
-    encode(image_input_path, encode_text, image_output_path)
-
+    encoded_image = encode(image_input_path, encode_text, image_output_path)
+    decode("", encoded_image)
